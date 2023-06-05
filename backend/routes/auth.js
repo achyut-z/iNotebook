5dc2eeb6932a8f6f46925f6d3f290a5000ff0f5b
Achyut Z <achyut.zalavadia@visilean.com>
2023-06-05 18:58:58 +0530

    Added express validator to validate user fields

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router()
+const { body, validationResult } = require('express-validator')
 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body)
-    user.save()
-    res.send(req.body);
+router.post('/', [
+    body('name', 'Name must be atleast 3 characters').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user))
+    .catch(error => {console.log(error)
+    res.json({error: "Please enter another email. This email is already in use", message: error.message})})
 })
 
 module.exports = router
